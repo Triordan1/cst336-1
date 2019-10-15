dc8a4225db5beda4633ed60ed9394828faa79d4c
EC2 Default User <ec2-user@ip-172-31-43-246.us-east-2.compute.internal>
2019-10-15 06:03:52 +0000

    added fancy colors

--- public/Project3/js/scripts.js
+++ public/Project3/js/scripts.js
@@ -40,7 +40,7 @@ $(document).ready(function(){
                     url: apiCall,
                     dataType: "json",
                     success: function(result,status){
-                        $("#weatherIcon").append("<img src = http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x.png>");
+                        $("#weatherIcon").html("<img src = http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x.png>");
                         $("#weather").html("The current weather in " + result.name + " is " + result.weather[0].description);
                         let kelvin = result.main.temp;
                         let farenheit = (kelvin - 273.15) *(9/5)+32;
@@ -58,7 +58,7 @@ $(document).ready(function(){
                     url: apiCall,
                     dataType: "json",
                     success: function(result,status){
-                        $("#weatherIcon").append("<img src = http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x.png>");
+                        $("#weatherIcon").html("<img src = http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x.png>");
                         $("#weather").html("The current weather in " + result.name + " is " + result.weather[0].description);
                         let kelvin = result.main.temp;
                         let farenheit = (kelvin - 273.15) *(9/5)+32;
@@ -76,7 +76,7 @@ $(document).ready(function(){
                     url: apiCall,
                     dataType: "json",
                     success: function(result,status){
-                        $("#weatherIcon").append("<img src = http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x.png>");
+                        $("#weatherIcon").html("<img src = http://openweathermap.org/img/wn/" + result.weather[0].icon + "@2x.png>");
                         $("#weather").html("The current weather in " + result.name + " is " + result.weather[0].description);
                         let kelvin = result.main.temp;
                         let farenheit = (kelvin - 273.15) *(9/5)+32;
@@ -88,23 +88,23 @@ $(document).ready(function(){
             }
             function tempIcon(temp){
                 if(temp > 100){
-                    $("#tempIcon").append("<img src = img/hellahot.jpg>");
+                    $("#tempIcon").html("<img src = img/hellahot.jpg>");
                     
                 }
                 else if(temp < 100 && temp > 79){
-                    $("#tempIcon").append("<img src = img/hot.png>");
+                    $("#tempIcon").html("<img src = img/hot.png>");
                 }
                 else if(temp < 80 && temp > 59){
-                    $("#tempIcon").append("<img src = img/nice.png>");
+                    $("#tempIcon").html("<img src = img/nice.png>");
                 }
                 else if(temp < 60 && temp > 39){
-                    $("#tempIcon").append("<img src = img/cool.png>");
+                    $("#tempIcon").html("<img src = img/cool.png>");
                 }
                 else if(temp < 40 && temp > 0){
-                    $("#tempIcon").append("<img src = img/snowing.png>");
+                    $("#tempIcon").html("<img src = img/snowing.png>");
                 }
                 else if(temp < 0){
-                    $("#tempIcon").append("<img src = img/hellacold.png>");
+                    $("#tempIcon").html("<img src = img/hellacold.png>");
                 }
                 
             }
